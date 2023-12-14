9d755fff486af8f8fb7a7e269084c5fdf7b5d9b3
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-14 13:33:10 -0500

    use base url when fetching

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,10 @@
 import useSWR from 'swr'
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const apiUrl = process.env.BASE_URL;
 
 export default function useFetch(endpoint) {
-  const { data, error } = useSWR(`${apiUrl}/${endpoint}`, fetcher);
+  const { data, error } = useSWR(`${apiUrl}/api/v1/${endpoint}`, fetcher);
 
   return {
     data,
