0a6ae161ad064f7e440a5704e007db2580289f94
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-14 13:55:38 -0500

    move reading of env var into function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,9 +1,10 @@
 import useSWR from 'swr'
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-const apiUrl = process.env.BASE_URL;
 
 export default function useFetch(endpoint) {
+  const apiUrl = process.env.BASE_URL;
+
   console.log('BASE_URL: ', apiUrl);
   const { data, error } = useSWR(`${apiUrl}/api/v1/${endpoint}`, fetcher);
 
