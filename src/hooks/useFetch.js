2cbcacf9ad90000e7be87413d33db7e522e51274
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-14 14:03:20 -0500

    revert to using NEXT_PUBLIC_API_URL

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,10 +3,10 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function useFetch(endpoint) {
-  const apiUrl = process.env.BASE_URL;
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  console.log('BASE_URL: ', apiUrl);
-  const { data, error } = useSWR(`${apiUrl}/api/v1/${endpoint}`, fetcher);
+  console.log('NEXT_PUBLIC_API_URL: ', apiUrl);
+  const { data, error } = useSWR(`${apiUrl}/${endpoint}`, fetcher);
 
   return {
     data,
