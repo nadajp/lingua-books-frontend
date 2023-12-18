9a5497bb220d256a5bb67f010d76c792396dd758
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-18 16:52:25 -0500

    remove scopes

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -3,9 +3,8 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 export default handleAuth({
     login: handleLogin({
       authorizationParams: {
-        audience: 'https://lingua.books.com', // or AUTH0_AUDIENCE
+        audience: process.env.AUTH0_AUDIENCE, // or AUTH0_AUDIENCE
         // Add the `offline_access` scope to also get a Refresh Token
-        scope: 'openid profile email' // or AUTH0_SCOPE
       }
     })
   });
