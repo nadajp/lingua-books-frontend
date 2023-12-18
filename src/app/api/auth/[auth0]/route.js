e1577ac792bbd087f78f0d3ddc1956bcb9cb6761
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-18 16:49:27 -0500

    update auth0 audience

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,11 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export default handleAuth({
+    login: handleLogin({
+      authorizationParams: {
+        audience: 'https://lingua.books.com', // or AUTH0_AUDIENCE
+        // Add the `offline_access` scope to also get a Refresh Token
+        scope: 'openid profile email' // or AUTH0_SCOPE
+      }
+    })
+  });
