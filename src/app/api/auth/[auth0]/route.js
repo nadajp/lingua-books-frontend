73a3239431454891370ff9ed75ea4f7420899399
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-18 15:49:35 -0500

    auth login change

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,17 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export default handleAuth({
+    async login(req, res) {
+        try {
+          await handleLogin(req, res, {
+            authorizationParams: {
+              audience: process.env.AUTH0_AUDIENCE, 
+              // Add the `offline_access` scope to also get a Refresh Token 
+              scope: 'openid profile offline_access' 
+            }
+          });
+        } catch (error) {
+          res.status(error.status || 400).end(error.message);
+        }
+      }
+});
