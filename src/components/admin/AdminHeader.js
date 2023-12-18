81bd46ebd7055ceb14cf7ab90072a718460c7faf
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-18 15:55:59 -0500

    revert auth change

--- src/components/admin/AdminHeader.js
+++ src/components/admin/AdminHeader.js
@@ -3,7 +3,6 @@ export default function AdminHeader() {
     <header className="bg-white shadow py-4">
       <div className="container mx-auto px-4 flex justify-between">
         <span className="font-semibold text-xl">Admin Dashboard</span>
-        <button className="text-blue-600 hover:text-blue-800">Login/Logout</button>
       </div>
     </header>
   );
