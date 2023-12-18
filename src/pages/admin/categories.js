104002199de2994f164ddadb08b29dc963eff811
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-12-18 14:26:01 -0500

    update categories

--- src/pages/admin/categories.js
+++ src/pages/admin/categories.js
@@ -1,12 +1,16 @@
 import CategoriesTable from '../../components/admin/Categories/CategoriesTable';
 import AdminLayout from '../../components/admin/AdminLayout';   
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
-export default withApiAuthRequired(function CategoriesPage() {
+export default withPageAuthRequired(function CategoriesPage() {
+  const { user, isLoading } = useUser();
+  
+  if (isLoading) return <div className="text-white">Loading...</div>;
+
   return (
     <AdminLayout>
       <h1 className="text-2xl font-semibold mb-4">Categories</h1>
       <CategoriesTable />
     </AdminLayout>
   );
-});
+})
